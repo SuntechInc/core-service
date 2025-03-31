47f760db7cbb12fbbd0ebec1c6c3b711b59695d9
Lucas Previato <lucaspreviato@Live.com>
2025-03-30 23:58:22 -0300

    feat: add Fastify, health endpoint, k8s manifests

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule,
+    new FastifyAdapter(),
+  );
+
+
+  const port = process.env.PORT || 3334;
+  await app.listen(port, '0.0.0.0');
+  console.log(` Core Service is running on Fastify! Port: ${port}`);
 }
 bootstrap();
